807d6b40cc7c617fdb5a7b7251310e7a0b345398
Alchemy <Alchemy@192.168.1.227>
2020-01-08 10:31:42 +0530

    ctstr

--- 7jan2020--2/src/comm/example/HoverCraft.java
+++ 7jan2020--2/src/comm/example/HoverCraft.java
@@ -2,12 +2,26 @@ package comm.example;
 
 public class HoverCraft extends Transport 
 {
-	public void calcDistance()
+	double claculateDistance(double initialdistance,double finaldistance)
 	{
-		
+		setDistance(finaldistance-initialdistance);
+		return getDistance();
 	}
-	public void calcFuelEff()
+	@Override
+	double calculateEfficiency(double initialfuel, double finalfuel)
 	{
-		
+		setEfficiency(getDistance()/(initialfuel-finalfuel));
+		return getEfficiency();
+	}
+	@Override
+	public String toString()
+	{
+		return"Hover Craft's"+super.toString();
+	}
+	@Override
+	double calculateDistance(double initialdistance, double finaldistance) {
+		// TODO Auto-generated method stub
+		setDistance(finaldistance-initialdistance);
+		return getDistance();
 	}
 }
--- 7jan2020--2/src/comm/example/MyApp1.java
+++ 7jan2020--2/src/comm/example/MyApp1.java
@@ -9,9 +9,14 @@ public class MyApp1 {
 		// +card1.getSuit().getRank());
 		 
 		
-		Car car=new Sedan("2019", "Blue", 2000000, true);
-		System.out.println(car);
-		
+		//Car car=new Sedan("2019", "Blue", 2000000, true);
+		//System.out.println(car);
+		Transport t=new Truck();
+		t.calculateDistance(2000, 2500);
+		t.calculateEfficiency(500, 400);
+		Transport w= new HoverCraft();
+		w.calculateDistance(400, 450);
+		w.calculateEfficiency(300, 100);
 
 	}
 
