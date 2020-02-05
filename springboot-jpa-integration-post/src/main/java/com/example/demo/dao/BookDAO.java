ac47f095764e47ceb639f4d21f53aa4f9c197749
Alchemy <Alchemy@192.168.1.188>
2020-02-05 17:14:20 +0530

    post find by name commit

--- springboot-jpa-integration-post/src/main/java/com/example/demo/dao/BookDAO.java
+++ springboot-jpa-integration-post/src/main/java/com/example/demo/dao/BookDAO.java
@@ -1,5 +1,7 @@
 package com.example.demo.dao;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +11,13 @@ import com.example.demo.entity.Book;
 @Repository
 @Transactional
 public interface BookDAO extends JpaRepository<Book, Integer>{
+
+	public List<Book> findByAuthor(String author);
+
+	public List<Book> findByBookName(String bookName);
+
+	public List<Book> findByAuthorAndBookName(String author, String bookName);
+	
+	public void deleteByAuthor(String author);
 
 }
--- springboot-jpa-integration-post/src/main/java/com/example/demo/service/BookServiceImpl.java
+++ springboot-jpa-integration-post/src/main/java/com/example/demo/service/BookServiceImpl.java
@@ -16,26 +16,22 @@ import com.example.demo.entity.Book;
 public class BookServiceImpl implements BookService {
 
 	private BookDAO bookDAO;
-	
-	
 	@Autowired
 	
 	public BookServiceImpl(BookDAO bookDAO) {
 		super();
 		this.bookDAO = bookDAO;
 	}
-
+	
 	@Override
 	@Transactional
 	public List<Book> getAllBooks() {
-		// TODO Auto-generated method stub
 		return bookDAO.findAll();
 	}
 
 	@Override
 	@Transactional
 	public Optional<Book>  getBookById(Integer bookId) {
-		
 		Optional<Book> book=bookDAO.findById(bookId);
 		
 		return book;
@@ -43,8 +39,44 @@ public class BookServiceImpl implements BookService {
 
 	@Override
 	public Book createBook(Book book) {
-		// TODO Auto-generated method stub
 		return bookDAO.save(book);
 	}
+
+	@Override
+	public void deleteBookById(Integer bookId) {
+		bookDAO.deleteById(bookId);
+		
+	}
+
+	@Override
+	public void deleteAll() {
+		bookDAO.deleteAll();
+		
+	}
+	@Override
+	public List<Book> findByAuthor(String author) {
+		// TODO Auto-generated method stub
+		return bookDAO.findByAuthor(author);
+	}
+
+	@Override
+	public List<Book> findByBookName(String bookName) {
+		// TODO Auto-generated method stub
+		return bookDAO.findByBookName(bookName);
+	}
+
+	@Override
+	public List<Book> findByAuthorAndBookName(String author, String bookName) {
+		// TODO Auto-generated method stub
+		return bookDAO.findByAuthorAndBookName(author, bookName);
+	}
+
+	@Override
+	public void deleteByAuthor(String author) {
+		// TODO Auto-generated method stub
+		bookDAO.deleteByAuthor(author);
+		
+	}
+
 
 }
